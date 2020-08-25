b91fb53fcdff99f5c42bfa2d164c3f2079adb925
Uditkumar13 <uditkumar2401@gmail.com>
2020-08-25 11:12:14 +0530

    explanation for the answer

--- Foundation/1.Basic of Programming/Getting started/postinc_preinc_question.java
+++ Foundation/1.Basic of Programming/Getting started/postinc_preinc_question.java
@@ -18,3 +18,13 @@ public static void main(String[] args) {
 
  }
 }
+
+/*part1
+The if condition in following code processes left to right
+i++ changes the value of i to 11, but returns old value which is 10. This is compared by == to i (the current value at i's location, which is 11)
+Hence the condition is false and we get 11 is bad.
+part2
+The if condition in following code processes left to right
+++j changes the value of j to 21, and returns new value which is 21. This is compared by == to j (the current value at j's location, which is 21)
+Hence the condition is true and we get 21 is bad.
+*/
