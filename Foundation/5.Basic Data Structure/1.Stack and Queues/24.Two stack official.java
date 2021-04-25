452e0e2c620b58944e578c2bdc4c765973628d22
Uditkumar13 <uditkumar2401@gmail.com>
2021-04-25 19:03:05 +0530

    Two stack

--- Foundation/5.Basic Data Structure/1.Stack and Queues/24.Two stack official.java
+++ Foundation/5.Basic Data Structure/1.Stack and Queues/24.Two stack official.java
@@ -5,94 +5,103 @@ public class Main {
 
   public static class TwoStack {
     int[] data;
-     Stack<Integer> tos1 = new Stack<>();
-     Stack<Integer> tos2 = new Stack<>();
+    int tos1;
+    int tos2;
+  
 
     public TwoStack(int cap) {
       // write your code here
       data = new int [cap] ; 
+      tos1 =-1;
+      tos2 = data.length;
     
     }
 
     int size1() {
       // write your code here
-      return tos1.size();
+      return tos1 + 1 ;
     }
 
     int size2() {
       // write your code here
-      return tos2.size();
+      return data.length - tos2 ;
     }
 
     void push1(int val) {
       // write your code here
-      if ((tos1.size() + tos2.size() ) == data.length){
+      if (tos2 == tos1 + 1){
           System.out.println("Stack overflow");
           return ;
       }
       
-      else{
-          tos1.push(val);
+      else{ tos1 ++ ;
+           data[tos1] = val;
       }
     }
 
     void push2(int val) {
       // write your code here
-      if ((tos1.size() + tos2.size() ) == data.length){
+      if (tos2 == tos1 + 1){
           System.out.println("Stack overflow");
           return ;
       }
       
-      else{
-          tos2.push(val);
+      else{tos2 -- ;
+          data[tos2] = val ;
       }
     }
 
     int pop1() {
       // write your code here
-      if (tos1.size()==0){
+      if (size1()==0){
           System.out.println("Stack underflow");
           return -1;
       }
       
       else{
-          return tos1.pop();
+          int val = data[tos1];
+          tos1 -- ;
+          return val;
       }
     }
 
     int pop2() {
       // write your code here
-      if (tos2.size()==0){
+      if (size2()==0){
           System.out.println("Stack underflow");
           return -1;
       }
       
       else{
-          return tos2.pop();
+          int val = data[tos2];
+          tos2++;
+          return val;
       }
     }
 
     int top1() {
       // write your code here
-      if (tos1.size()==0){
+      if (size1()==0){
           System.out.println("Stack underflow");
           return -1;
       }
       
       else{
-          return tos1.peek();
+          int val = data[tos1];
+          return val;
       }
     }
 
     int top2() {
       // write your code here
-      if (tos2.size()==0){
+      if (size2()==0){
           System.out.println("Stack underflow");
           return -1;
       }
       
       else{
-          return tos2.peek();
+          int val = data[tos2];
+          return val;
       }
     }
   }
