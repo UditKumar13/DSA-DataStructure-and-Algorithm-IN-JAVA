529632eed7992225dd191ac92d7c12440beeb1b6
Uditkumar13 <uditkumar2401@gmail.com>
2021-08-11 15:44:28 +0530

    Queen Permutaion 2d vs 2d box chooses

---
+++ INTERMEDIATE_LEVEL_2/1.Conceptual Building Blocks/1.Recursion and Backtracking/27.Queens Permutaions 2d as 2d box chooses.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void queensPermutations(int qpsf, int tq, int row, int col, String asf, boolean[] queens) {
+        
+        
+        if (row == tq){
+            if(qpsf == tq){
+                System.out.println(asf) ; 
+                System.out.println(); 
+            }
+            
+            return  ;
+            
+        }
+        //base case 
+        
+        int nr = 0 ;
+        int nc = 0 ;
+        String sep  = " " ; 
+        if (col == tq-1){
+            nc  =0 ;
+            nr = row + 1 ;
+            sep = "\n";
+        }
+        
+        else{
+            nr = row ; 
+            nc = col + 1 ;
+            sep = "\t" ; 
+        }
+        for (int i=0; i<queens.length;i++){
+                if (queens[i] == false ){
+                    queens[i] = true ;
+                    queensPermutations(qpsf+1,tq,nr,nc,asf +"q"+(i+1)+sep , queens) ; 
+                    queens[i] = false ; 
+        }
+    }
+    queensPermutations(qpsf,tq,nr,nc,asf +"-"+sep , queens) ;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] queens = new boolean[n];
+
+        queensPermutations(0, n, 0, 0, "", queens);
+    }
+}
