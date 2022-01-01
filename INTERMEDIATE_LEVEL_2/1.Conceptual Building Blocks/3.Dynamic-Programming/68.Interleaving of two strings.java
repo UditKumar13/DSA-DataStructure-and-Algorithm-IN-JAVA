4cf8dce1804fb5f32f205509d22dfd7b35ff423f
UditKumar13 <uditkumar2401@gmail.com>
2022-01-01 21:14:48 +0530

    Interleaving of two strings

---
+++ INTERMEDIATE_LEVEL_2/1.Conceptual Building Blocks/3.Dynamic-Programming/68.Interleaving of two strings.java
@@ -0,0 +1,76 @@
+/*
+
+1. You are given three strings - s1, s2 and s3.
+2. You have to find whether s3 is formed by interleaving of s1 and s2.
+3. s3 is interleaving if it contains all characters of s1 and s2, and order of all characters in individual string is preserved.
+
+
+Input :
+aabcc
+dbbca
+aadbbcbcac
+
+
+Output :
+true
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+
+// Approach 1 : Recursive approach 
+
+public class Main {
+
+  public static boolean solution(String s1, String s2, String s3,int i, int j ,boolean [][] dp ) {
+    if (i == s1.length () && j == s2.length()){
+      return true ; 
+    }
+    
+    
+    if (dp[i][j] != null){
+      return dp[i][j] ; 
+    }
+    
+    
+    
+    if (i<s1.length () && s1.charAt(i) == s3.charAt(i+j)){
+      boolean f1 = solution (s1,s2,s3,i+1,j,dp) ; 
+      dp[i][j] = f1 ; 
+      if (f1 == true){
+        return true ; 
+      }
+    }
+    
+    
+    if (j<s2.length() && s2.charAt(j) == s3.charAt(i+j)){
+      boolean f2 = solution(s1,s2,s3,i,j+1,dp) ;
+      dp[i][j] = f2; 
+      if (f2 == true){
+        return true ; 
+      }
+      
+      
+    }
+    
+    return false ; 
+  
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String s1 = scn.nextLine();
+    String s2 = scn.nextLine();
+    String s3 = scn.nextLine();
+    
+    if (s1.length() + s2.length() != s3.length()){
+      System.out.print("false") ; 
+      return ; 
+    }
+    
+    System.out.println(solution(s1, s2, s3,0,0, new Boolean [s1.length() + 1][s2.length() + 1]));
+  }
+
+}
